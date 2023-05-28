a3d7f97342f99e364cc27e0df14f52ae4977a1b8
zmirgopin <zahavagopin@gmail.com>
2023-05-27 23:18:16 -0400

    week 1

--- week-1/hello-world/src/app/app.component.ts
+++ week-1/hello-world/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  myWorld = "Your'e now in Zahava's world";
+  myWorld = "You're now in Zahava's world";
 }
