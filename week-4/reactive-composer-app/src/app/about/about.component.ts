c15b5fd323083f9b4dde767d13918083c84cbb35
zmirgopin <zahavagopin@gmail.com>
2023-06-14 12:19:08 -0400

    week 4

---
+++ week-4/reactive-composer-app/src/app/about/about.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Title: about.component.ts
+ * Author: Zahava Gopin
+ * Date: 1 June 2023
+ * Description: About component (About Us page)
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
