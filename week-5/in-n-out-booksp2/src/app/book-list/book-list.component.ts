c0f6e6d2ee6bb71ee594014e781dba8a20ef4d30
zmirgopin <zahavagopin@gmail.com>
2023-06-23 12:05:16 -0400

    week 5

---
+++ week-5/in-n-out-booksp2/src/app/book-list/book-list.component.ts
@@ -0,0 +1,35 @@
+/**
+ * Title: book-list.component.ts
+ * Author: Zahava Gopin
+ * Date: 23 June 2023
+ * Description: Book list component (book list page)
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { BooksService } from '../books.service';
+import { IBook } from '../book.interface';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books: Observable<IBook[]>;
+  header: Array<string> = ['isbn', 'title', 'numOfPages', 'authors']
+  book: IBook;
+
+  constructor(private booksService: BooksService) {
+    this.books = this.booksService.getBooks();
+  }
+
+  ngOnInit(): void {
+  }
+
+  showBookDetails(isbn: string) {
+    this.book = this.booksService.getBook(isbn);
+    console.log(this.book);
+  }
+}
