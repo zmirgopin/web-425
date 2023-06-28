3c2cf7bcf2c96cc042b961b57ceaf1131246042d
zmirgopin <zahavagopin@gmail.com>
2023-06-28 15:35:09 -0400

    git commit

---
+++ week-6/gpa-calculator-app1/src/app/base-layout/base-layout.component.ts
@@ -0,0 +1,24 @@
+/**
+ * Title: base-layout.component.ts
+ * Author: Zahava Gopin
+ * Date: 28 June 2023
+ * Description: Base layout (Base page)
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-layout',
+  templateUrl: './base-layout.component.html',
+  styleUrls: ['./base-layout.component.css']
+})
+export class BaseLayoutComponent implements OnInit {
+
+  assignment: string;
+
+  constructor() {
+    this.assignment = 'Exercise 6.3 - Layouts';
+  }
+  ngOnInit(): void {
+  }
+
+}
