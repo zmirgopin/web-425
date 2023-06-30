722af26af4920cc62864485813b7c27d6c32b126
zmirgopin <zahavagopin@gmail.com>
2023-06-30 11:02:52 -0400

    git commit

---
+++ week-6/gpa-calculator-app2/src/app/gpa/gpa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-gpa',
+  templateUrl: './gpa.component.html',
+  styleUrls: ['./gpa.component.css']
+})
+export class GpaComponent implements OnInit {
+  @Input() gpaTotal: number;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ week-6/gpa-calculator-app2/src/app/not-found/not-found.component.ts
@@ -0,0 +1,21 @@
+/**
+ * Title: not-found.component.ts
+ * Author: Zahava Gopin
+ * Date: 28 June 2023
+ * Description: Not Found layout
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
