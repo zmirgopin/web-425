f86f85328130f70ac2c448b58949fb76980d4fd3
zmirgopin <zahavagopin@gmail.com>
2023-07-10 20:07:02 -0400

    git commit

---
+++ week-8/in-n-out-books/src/app/wishlist-create/wishlist-create.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WishlistCreateComponent } from './wishlist-create.component';
+
+describe('WishlistCreateComponent', () => {
+  let component: WishlistCreateComponent;
+  let fixture: ComponentFixture<WishlistCreateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WishlistCreateComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WishlistCreateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
